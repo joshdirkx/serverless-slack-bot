73211d6a700d1c00c7f35d8cf15637b281048a4c
Josh Dirkx <joshdirkx@gmail.com>
2023-02-24 19:06:57 -0800

    commit

--- bin/serverless-slack-bot.ts
+++ bin/serverless-slack-bot.ts
@@ -1,21 +1,13 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
+
+import { App } from 'aws-cdk-lib';
 import { ServerlessSlackBotStack } from '../lib/serverless-slack-bot-stack';
 
-const app = new cdk.App();
-new ServerlessSlackBotStack(app, 'ServerlessSlackBotStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
+const app = new App();
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+new ServerlessSlackBotStack(app, 'SimpleMemeServiceStack', {
+  env: {
+    account: '245824979453',
+    region: 'us-west-2',
+  },
 });
--- lib/serverless-slack-bot-stack.ts
+++ lib/serverless-slack-bot-stack.ts
@@ -1,16 +1,41 @@
-import * as cdk from 'aws-cdk-lib';
+import { CfnOutput, CfnParameter, Stack, StackProps } from 'aws-cdk-lib';
+import { LambdaIntegration, RestApi } from 'aws-cdk-lib/aws-apigateway';
+import { Architecture, Code, Function, Handler, Runtime } from 'aws-cdk-lib/aws-lambda';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
-export class ServerlessSlackBotStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+var path = require('path');
+
+export class ServerlessSlackBotStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const slackToken = new CfnParameter(this, 'slackToken', {
+      type: "String",
+      description: "Token for your Slack Bot",
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'ServerlessSlackBotQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
-  }
-}
+    const lambda = new Function(this, 'simpleMemeServiceHandler', {
+      code: Code.fromAssetImage(path.join(__dirname, '..', 'lambda')),
+      handler: Handler.FROM_IMAGE,
+      runtime: Runtime.FROM_IMAGE,
+      architecture: Architecture.ARM_64,
+      environment: {
+        SLACK_TOKEN: slackToken.valueAsString,
+      },
+    });
+
+    const api = new RestApi(this, 'api');
+
+    const lambdaApi = api.root.addResource('lambda');
+
+    const lambdaIntegration = new LambdaIntegration(lambda, {
+      proxy: true,
+    });
+
+    lambdaApi.addMethod("POST", lambdaIntegration);
+
+    new CfnOutput(this, 'simpleMemeServiceHandlerUrl', {
+      value: api.url,
+    });
+  };
+};
